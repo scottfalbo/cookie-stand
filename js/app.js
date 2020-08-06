71a89fd552c06d4d3bce1d47518db1a0ff2eed77
scottfalbo <scottfalboart@gmail.com>
2020-08-06 13:30:21 -0700

    got my methods to return the proper array of info.

--- js/app.js
+++ js/app.js
@@ -4,11 +4,27 @@ var seattle = {
   minCust: 23,
   maxCust: 65,
   avgSale: 6.3,
+  openTime: 6,
+  closeTime: 20,
+  // generates a random number of customers within the min, max range
   custPerHour: function(){
-    return Math.floor(Math.random() * 71);
+    var custPer = Math.floor(Math.random() * 101);
+    while (custPer < this.minCust || custPer > this.maxCust){
+      custPer = Math.floor(Math.random() * 101);
+    }
+    return custPer;
+  },
+  // returns an array with two arrays inside.  The inner arrays hold the hour of the day and the number of cookies sold in related index ids
+  cookiesPerHour: function(){
+    var perHour = [];
+    var hour = [];
+    for (var i = this.openTime; i < this.closeTime; i++){
+      perHour[i - this.openTime] = (this.custPerHour() * this.avgSale).toFixed(0);
+      hour[i - this.openTime] = i;
+    }
+    return [hour, perHour];
   }
-  //calculate and store cookies purchased per hour based on custPerHour and avgSale
-  //store results in an array
 };
 
-//display the array to the browser
+
+//display the array in the browser
