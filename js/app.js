b62909662655f7f97bc4ffe0d23b43f92f228ba2
scottfalbo <scottfalboart@gmail.com>
2020-08-06 12:18:35 -0700

    wired index and sales pages together

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
 'use strict';
 
+var seattle = {
+  minCust: 23,
+  maxCust: 65,
+  avgSale: 6.3,
+  custPerHour: function(){
+    return Math.floor(Math.random() * 71);
+  }
+  //calculate and store cookies purchased per hour based on custPerHour and avgSale
+  //store results in an array
+};
+
+//display the array to the browser
